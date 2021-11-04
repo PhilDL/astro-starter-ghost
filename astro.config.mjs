cb58fec87789edb0b9ca9f5c726927f17ab1b588
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2021-11-04 14:40:50 +0400

    [WIP] Migration to astro next
    Auto stash before rebase of "refs/heads/astro-next"
    [FIX] Script.js url
    
    [IMP] Props type definitions
    
    
    [IMP] Fix typescript errors on index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,5 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
-
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages',   // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
     site: "https://mysite.com", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true, // Generate sitemap (set to "false" to disable)
@@ -21,4 +9,12 @@ export default /** @type {import('astro').AstroUserConfig} */ {
     // tailwindConfig: './tailwind.config.js',
   },
   renderers: ["@astrojs/renderer-svelte"],
+  vite: {
+    resolve: {
+      alias: {
+        $: "/src",
+        $components: "/src/components",
+      },
+    },
+  },
 };
