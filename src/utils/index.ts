187e7730b9ed6eb6735a6b79778227065fa26672
PhilDL <philippe.lattention@hotmail.fr>
2021-11-04 18:36:19 +0400

    [IMP] Handling ghost demo data url

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,9 @@ export const getGhostImgPath = (
   imgUrl: string,
   width: number = 0
 ): string => {
+  if (!imgUrl.startsWith(baseUrl)) {
+    return imgUrl;
+  }
   const relativePath = imgUrl.substring(`${baseUrl}/content/images`.length);
   const cleanedBaseUrl = baseUrl.replace(/\/$/, "");
   if (width && width > 0) {
@@ -11,11 +14,10 @@ export const getGhostImgPath = (
   return `${cleanedBaseUrl}/content/images/${width}${relativePath}`;
 };
 
-
 export const truncate = (input: string, size: number): string =>
   input.length > size ? `${input.substring(0, size)}...` : input;
 
-export const formatDate = (dateInput: string): string=> {
+export const formatDate = (dateInput: string): string => {
   let dateObject = new Date(dateInput);
   return dateObject.toDateString();
 };
