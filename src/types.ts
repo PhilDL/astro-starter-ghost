f1e6a9545ed487d6c00168b8cd984932fb00563d
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2023-03-09 14:11:18 +0400

    cleanup

--- src/types.ts
+++
@@ -1,7 +0,0 @@
-import type { fetchSettings } from "./utils/ghost-helpers";
-export type NavigationLink = {
-  label: string;
-  url: string;
-};
-
-export type Settings = Awaited<ReturnType<typeof fetchSettings>>;
